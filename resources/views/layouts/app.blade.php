c3e73e21d565630534b32ee54a7128344f4e2e1c
Nikita Malkin <demeja16@gmail.com>
2021-06-11 17:01:54 +0300

    initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="{{ mix('images/favicon.png') }}" type="image/x-icon"/>
+    <title>@yield('SeoTitle')</title>
+    <link rel="stylesheet" href="{{ mix('css/main.css') }}">
+</head>
+<body>
+    @yield('content')
+
+    <script src="{{ mix('js/main.js') }}"></script>
+</body>
+</html>
