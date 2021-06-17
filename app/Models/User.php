ae29fb707dcbd4452a60d739171b73b38111d98b
Malkin Nikita <startted@iMac-Startted.local>
2021-06-17 17:55:09 +0300

    add articles + add laravel/sluggable

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,23 +11,17 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password',
     ];
+
+    public function articles() {
+        return $this->hasMany(Article::class);
+    }
 }
