0c81d308ef8f41809a5e01b0a44d275486964d25
Malkin NikitOS <demeja16@gmail.com>
2021-06-23 15:22:22 +0300

    fixes

--- app/Enum/SettingsEnum.php
+++ app/Enum/SettingsEnum.php
@@ -6,11 +6,13 @@ class SettingsEnum
 {
     public const comments_depth = 'comments_depth';
     public const comments_pagination = 'comments_pagination';
+    public const articles_pagination = 'articles_pagination';
 
     public static function values() {
         return [
             self::comments_depth => 1,
             self::comments_pagination => 20,
+            self::articles_pagination => 20,
         ];
     }
 }
--- app/Repositories/ArticleRepository.php
+++ app/Repositories/ArticleRepository.php
@@ -6,6 +6,10 @@ use App\Models\Article;
 
 class ArticleRepository
 {
+    public function articleList() {
+        return Article::query()->with(['categories', 'tags', 'images'])->withCount(['comments', 'likes']);
+    }
+
     public function singlePageArticle(int $article_id)
     {
         return Article::byId($article_id)
