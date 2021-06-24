1354480ba41200567bd1729ee7d23e291b66be1a
TheFatRat <valerij.kamlev@startted.com>
2021-06-24 15:15:44 +0300

    mobile menu

--- resources/assets/scripts/components/header/init.js
+++ resources/assets/scripts/components/header/init.js
@@ -1,7 +1,21 @@
 export default function header() {
+    let body = document.querySelector('body');
+    let mobileMenu = document.querySelector('.mobile-menu');
+    let closeMobileMenuButton = document.querySelector('.mobile-menu__close');
     let searchMobileButton = document.querySelector('.header-top__search-mobile');
+    let hamburger = document.querySelector('.header-top__hamburger');
+
     searchMobileButton.addEventListener('click', function() {
-        console.log('click');
         document.querySelector('.header-top__search').classList.toggle('active');
+    });
+
+    hamburger.addEventListener('click', function() {
+        body.classList.add('overlay');
+        mobileMenu.classList.add('open');
+    });
+
+    closeMobileMenuButton.addEventListener('click', function () {
+        body.classList.remove('overlay');
+        mobileMenu.classList.remove('open');
     })
 }
---
+++ resources/views/components/mobile-menu/wrap.blade.php
@@ -0,0 +1,20 @@
+<div class="mobile-menu">
+    <div class="mobile-menu__wrapper">
+        <ul class="mobile-menu__menu">
+            <li>
+                <a href="#" class="text_bold mobile-menu__link">Статьи</a>
+            </li>
+            <li>
+                <a href="#" class="text_bold mobile-menu__link">Вопросы</a>
+            </li>
+        </ul>
+        <div class="mobile-menu__buttons">
+            <a href="" class="button button_light mobile-menu__button">Написать статью</a>
+            <a href="" class="button button_light mobile-menu__button">Задать вопрос</a>
+        </div>
+    </div>
+
+    <div class="mobile-menu__close">
+        @include('icons.close')
+    </div>
+</div>
