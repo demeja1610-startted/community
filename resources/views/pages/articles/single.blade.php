9892577d041cf55f3e2b9163693965caa9a15dc3
Malikov <nail.malikov@startted.com>
2021-06-24 10:18:41 +0300

    init

--- resources/views/pages/articles/single.blade.php
+++ resources/views/pages/articles/single.blade.php
@@ -2,10 +2,22 @@
 
 @section('content')
     <div class="container">
-        <div class="article-single">
-            <h1 class="article-single__title">{!! $article->title !!}</h1>
-            <img src="{!! $article->images->first()->url !!}" alt="{{ $article->images->first()->alt }}">
+        <div class="page-wrapper">
+
+            <main class="page-wrapper__content">
+                <div class="article-single">
+                    <h1 class="article-single__title">{!! $article->title !!}</h1>
+                    <img src="{!! $article->images->first()->url !!}" alt="{{ $article->images->first()->alt }}">
+                </div>
+
+                @include('components.comments/wrap', ['model' => $article, 'max_depth' => $max_depth, 'comments' => $comments])
+
+            </main>
+
+            <aside class="page-wrapper__sidebar">
+                @include('components.sidebar.wrap')
+            </aside>
+
         </div>
-        @include('components.comments/wrap', ['model' => $article, 'max_depth' => $max_depth, 'comments' => $comments])
     </div>
 @endsection
