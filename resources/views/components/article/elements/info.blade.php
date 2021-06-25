c018c69e629c21a43d2fc2b1ee0f1d21ebe1299a
Malikov <nail.malikov@startted.com>
2021-06-25 08:46:12 +0300

    icons src fix

--- resources/views/components/article/elements/info.blade.php
+++ resources/views/components/article/elements/info.blade.php
@@ -1,7 +1,7 @@
 <div class="article-info mrgn12-bottom">
 
     <div class="article-info__avatar">
-        <img src="{{ URL::asset('/images/icons/no-avatar.svg') }}" alt="" class="article-info__avatar-image">
+        <img src="{{ URL::asset('/images/no-avatar.svg') }}" alt="" class="article-info__avatar-image">
     </div>
 
     <a href="#" class="article-info__user">Inje</a>
@@ -9,7 +9,7 @@
     <div class="article-info__date">Вчера в 14:49</div>
 
     <div class="article-info__views">
-        {!! file_get_contents('images/icons/eye.svg') !!}
+        @include('icons.eye')
         269
     </div>
 
