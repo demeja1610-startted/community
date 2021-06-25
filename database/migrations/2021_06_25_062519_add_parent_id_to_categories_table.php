5a5c3f32d60e7e9802547a83e5d07b0ddf96d86a
Malkin NikitOS <demeja16@gmail.com>
2021-06-25 09:28:36 +0300

    add parent_id to categories

---
+++ database/migrations/2021_06_25_062519_add_parent_id_to_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddParentIdToCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->unsignedBigInteger('parent_id')->nullable()->after('slug');
+
+            $table->foreign('parent_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::hasColumn('categories', 'parent_id')) {
+            Schema::table('categories', function (Blueprint $table) {
+                $table->dropColumn('parent_id');
+                $table->dropForeign(['parent_id']);
+            });
+        }
+    }
+}
