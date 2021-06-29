5609dcb63c87659d109d5d6dc9da7599ec53ffae
TheFatRat <valerij.kamlev@startted.com>
2021-06-29 09:55:28 +0300

    add service to index page

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,12 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Article;
+use App\Services\ArticleService;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
+    protected $articleService;
+
+    public function __construct(ArticleService $articleService)
+    {
+        $this->articleService = $articleService;
+    }
+
     public function index() {
-        $articles = Article::get();
+        $articles = $this->articleService->index();
         return view('index', compact('articles'));
     }
 }
