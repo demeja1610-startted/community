bf5924ee794a4de1b215ad5565b3b0608968ee99
Malkin NikitOS <demeja16@gmail.com>
2021-06-30 15:20:30 +0300

    add Admin Routes Enum

---
+++ app/Enum/RouteNames/AdminRouteNamesEnum.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Enum\RouteNames;
+
+class AdminRouteNamesEnum {
+    public const page_index = 'page.admin.index';
+
+    public const page_articles_index = 'page.admin.articles.index';
+    public const page_articles_create = 'page.admin.articles.create';
+    public const page_articles_edit = 'page.admin.articles.edit';
+    public const page_articles_store = 'admin.articles.store';
+    public const page_articles_update = 'admin.articles.update';
+    public const page_articles_destroy = 'admin.articles.delete';
+
+    public const page_questions_index = 'page.admin.questions.index';
+    public const page_questions_create = 'page.admin.questions.create';
+    public const page_questions_edit = 'page.admin.questions.edit';
+    public const page_questions_store = 'admin.questions.store';
+    public const page_questions_update = 'admin.questions.update';
+    public const page_questions_destroy = 'admin.questions.delete';
+
+    public const page_categories_index = 'page.admin.categories.index';
+    public const page_categories_edit = 'page.admin.categories.edit';
+    public const page_categories_store = 'admin.categories.store';
+    public const page_categories_update = 'admin.categories.update';
+    public const page_categories_destroy = 'admin.categories.delete';
+
+    public const page_tags_index = 'page.admin.tags.index';
+    public const page_tags_edit = 'page.admin.tags.edit';
+    public const page_tags_store = 'admin.tags.store';
+    public const page_tags_update = 'admin.tags.update';
+    public const page_tags_destroy = 'admin.tags.delete';
+
+    public const page_comments_index = 'page.admin.comments.index';
+    public const page_comments_edit = 'page.admin.comments.edit';
+    public const page_comments_update = 'admin.comments.update';
+    public const page_comments_approve = 'admin.comments.approve';
+    public const page_comments_unapprove = 'admin.comments.unapprove';
+    public const page_comments_destroy = 'admin.comments.delete';
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Enum\PermissionsEnum;
+use App\Enum\RouteNames\AdminRouteNamesEnum;
 use App\Events\Message;
 use App\Http\Controllers\Admin\AArticleController;
 use App\Http\Controllers\Admin\ACategoryController;
@@ -13,7 +13,6 @@ use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\IndexController;
-use App\Http\Controllers\Pages\LKController;
 use App\Http\Controllers\Pages\LoginController;
 use App\Http\Controllers\Pages\RegisterController;
 use Illuminate\Http\Request;
@@ -70,54 +69,54 @@ Route::group(['prefix' => 'comments'], function () {
 });
 
 Route::group(['prefix' => 'admin', 'middleware' => 'admin'], function () {
-    Route::get('/', [AIndexController::class, 'index'])->name('admin.index');
+    Route::get('/', [AIndexController::class, 'index'])->name(AdminRouteNamesEnum::page_index);
 
     Route::group(['prefix' => 'articles'], function () {
-        Route::get('/', [AArticleController::class, 'index'])->name('page.admin.articles.index');
-        Route::get('/create', [AArticleController::class, 'create'])->name('page.admin.articles.create');
-        Route::get('/{article_id}', [AArticleController::class, 'edit'])->name('page.admin.articles.edit');
+        Route::get('/', [AArticleController::class, 'index'])->name(AdminRouteNamesEnum::page_articles_index);
+        Route::get('/create', [AArticleController::class, 'create'])->name(AdminRouteNamesEnum::page_articles_create);
+        Route::get('/{article_id}', [AArticleController::class, 'edit'])->name(AdminRouteNamesEnum::page_articles_edit);
 
-        Route::put('/store', [AArticleController::class, 'store'])->name('admin.articles.store');
-        Route::patch('/{article_id}', [AArticleController::class, 'update'])->name('admin.articles.update');
-        Route::delete('/{article_id}', [AArticleController::class, 'destroy'])->name('admin.articles.delete');
+        Route::put('/store', [AArticleController::class, 'store'])->name(AdminRouteNamesEnum::page_articles_store);
+        Route::patch('/{article_id}', [AArticleController::class, 'update'])->name(AdminRouteNamesEnum::page_articles_update);
+        Route::delete('/{article_id}', [AArticleController::class, 'destroy'])->name(AdminRouteNamesEnum::page_articles_destroy);
     });
 
     Route::group(['prefix' => 'questions'], function () {
-        Route::get('/', [AQuestionController::class, 'index'])->name('page.admin.questions.index');
-        Route::get('/create', [AQuestionController::class, 'create'])->name('page.admin.questions.create');
-        Route::get('/{question_id}', [AQuestionController::class, 'edit'])->name('page.admin.questions.edit');
+        Route::get('/', [AQuestionController::class, 'index'])->name(AdminRouteNamesEnum::page_questions_index);
+        Route::get('/create', [AQuestionController::class, 'create'])->name(AdminRouteNamesEnum::page_questions_create);
+        Route::get('/{question_id}', [AQuestionController::class, 'edit'])->name(AdminRouteNamesEnum::page_questions_edit);
 
-        Route::put('/store', [AQuestionController::class, 'store'])->name('admin.questions.store');
-        Route::patch('/{question_id}', [AQuestionController::class, 'update'])->name('admin.questions.update');
-        Route::delete('/{question_id}', [AQuestionController::class, 'destroy'])->name('admin.questions.delete');
+        Route::put('/store', [AQuestionController::class, 'store'])->name(AdminRouteNamesEnum::page_questions_store);
+        Route::patch('/{question_id}', [AQuestionController::class, 'update'])->name(AdminRouteNamesEnum::page_questions_update);
+        Route::delete('/{question_id}', [AQuestionController::class, 'destroy'])->name(AdminRouteNamesEnum::page_questions_destroy);
     });
 
     Route::group(['prefix' => 'categories'], function () {
-        Route::get('/', [ACategoryController::class, 'index'])->name('page.admin.categories.index');
-        Route::get('/{category_id}', [ACategoryController::class, 'edit'])->name('page.admin.categories.edit');
+        Route::get('/', [ACategoryController::class, 'index'])->name(AdminRouteNamesEnum::page_categories_index);
+        Route::get('/{category_id}', [ACategoryController::class, 'edit'])->name(AdminRouteNamesEnum::page_categories_edit);
 
-        Route::put('/store', [ACategoryController::class, 'store'])->name('admin.categories.store');
-        Route::patch('/{category_id}', [ACategoryController::class, 'update'])->name('admin.categories.update');
-        Route::delete('/{category_id}', [ACategoryController::class, 'destroy'])->name('admin.categories.delete');
+        Route::put('/store', [ACategoryController::class, 'store'])->name(AdminRouteNamesEnum::page_categories_store);
+        Route::patch('/{category_id}', [ACategoryController::class, 'update'])->name(AdminRouteNamesEnum::page_categories_update);
+        Route::delete('/{category_id}', [ACategoryController::class, 'destroy'])->name(AdminRouteNamesEnum::page_categories_destroy);
     });
 
     Route::group(['prefix' => 'tags'], function () {
-        Route::get('/', [ATagController::class, 'index'])->name('page.admin.tags.index');
-        Route::get('/{tag_id}', [ATagController::class, 'edit'])->name('page.admin.tags.edit');
+        Route::get('/', [ATagController::class, 'index'])->name(AdminRouteNamesEnum::page_tags_index);
+        Route::get('/{tag_id}', [ATagController::class, 'edit'])->name(AdminRouteNamesEnum::page_tags_edit);
 
-        Route::put('/store', [ATagController::class, 'store'])->name('admin.tags.store');
-        Route::patch('/{tag_id}', [ATagController::class, 'update'])->name('admin.tags.update');
-        Route::delete('/{tag_id}', [ATagController::class, 'destroy'])->name('admin.tags.delete');
+        Route::put('/store', [ATagController::class, 'store'])->name(AdminRouteNamesEnum::page_tags_store);
+        Route::patch('/{tag_id}', [ATagController::class, 'update'])->name(AdminRouteNamesEnum::page_tags_update);
+        Route::delete('/{tag_id}', [ATagController::class, 'destroy'])->name(AdminRouteNamesEnum::page_tags_destroy);
     });
 
     Route::group(['prefix' => 'comments'], function () {
-        Route::get('/', [ACommentController::class, 'index'])->name('page.admin.comments.index');
-        Route::get('/{comment_id}', [ACommentController::class, 'edit'])->name('page.admin.comments.edit');
+        Route::get('/', [ACommentController::class, 'index'])->name(AdminRouteNamesEnum::page_comments_index);
+        Route::get('/{comment_id}', [ACommentController::class, 'edit'])->name(AdminRouteNamesEnum::page_comments_edit);
 
-        Route::patch('/{comment_id}', [ACommentController::class, 'update'])->name('admin.comments.update');
-        Route::patch('/{comment_id}/approve', [ACommentController::class, 'approve'])->name('admin.comments.approve');
-        Route::patch('/{comment_id}/unapprove', [ACommentController::class, 'unapprove'])->name('admin.comments.unapprove');
-        Route::delete('/{comment_id}', [ACommentController::class, 'destroy'])->name('admin.comments.delete');
+        Route::patch('/{comment_id}', [ACommentController::class, 'update'])->name(AdminRouteNamesEnum::page_comments_update);
+        Route::patch('/{comment_id}/approve', [ACommentController::class, 'approve'])->name(AdminRouteNamesEnum::page_comments_approve);
+        Route::patch('/{comment_id}/unapprove', [ACommentController::class, 'unapprove'])->name(AdminRouteNamesEnum::page_comments_unapprove);
+        Route::delete('/{comment_id}', [ACommentController::class, 'destroy'])->name(AdminRouteNamesEnum::page_comments_destroy);
     });
 });
 
