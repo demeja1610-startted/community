735af0fca0b63092ed2b0e911a31fa98caa29c69
artem <artem@startted.com>
2021-07-01 18:08:23 +0300

    fix articles and answers page for another user

--- resources/views/lk/components/articles/wrap.blade.php
+++ resources/views/lk/components/articles/wrap.blade.php
@@ -1,10 +1,22 @@
 @if($articles->count())
     @dump($articles)
 @else
-    @component('lk.components.empty', ['title' => 'У вас пока нет ни одной статьи'])
+    @component('lk.components.empty')
+        @slot('title')
+            @currentUser
+            У вас пока нет ни одной статьи
+            @else
+                У этого пользователя пока нет ни одной публикации
+            @endif
+        @endslot
         @slot('text')
+            @currentUser
             Но если хотите чем-то поделиться в Community, то быстрее жмите кнопку ниже
             <a class="d-flex button_center button button_maxContent mt-4 button_light" href="">Написать статью</a>
+            @else
+                Но не переживайте на Community есть много чего <a class="text_link"
+                                                                  href="{{ route(webRouterNames()::page_articles_index) }}">почитать</a>
+            @endif
         @endslot
     @endcomponent
 @endif
